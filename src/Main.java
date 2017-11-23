4998e82d8629f8e111d481e6531b1c39972686e2
stanislavdev <dvstas00@gmail.com>
2017-11-23 10:38:18 +0200

    add RunChosenPattern class

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,9 @@
-import FactoryMethod.Creator.DagitalWatchMaker;
-import FactoryMethod.Creator.RomeWatchMaker;
-import FactoryMethod.Creator.WatchMaker;
-import FactoryMethod.Product.Watch;
-
 /**
  * Created by dvsta on 15.11.2017.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("-------Factory Method-------");
-        WatchMaker digitalWatchMaker = new DagitalWatchMaker();
-        WatchMaker romeWatchMaker = new RomeWatchMaker();
-        Watch digitalWatch = digitalWatchMaker.createWatch();
-        Watch romeWatch = romeWatchMaker.createWatch();
-        digitalWatch.showTime();
-        romeWatch.showTime();
+        RunChosenPattern runChosenPattern = new RunChosenPattern();
+        runChosenPattern.runFactoryMethod();
     }
 }
